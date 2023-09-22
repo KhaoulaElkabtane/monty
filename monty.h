dfd62e857f2b5e034f3898d767a54cd4681eed48
KhaoulaElkabtane <elkabtanekhaoula@gmail.com>
2023-09-22 14:15:27 -0700

    betty update

--- monty.h
+++ monty.h
@@ -20,9 +20,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -35,8 +35,8 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *str;
-        void (*f)(stack_t **stack, unsigned int nub);
+	char *str;
+	void (*f)(stack_t **stack, unsigned int nub);
 } instruction_t;
 
 extern stack_t *hd;
