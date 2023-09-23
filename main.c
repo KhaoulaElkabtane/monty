6c155358b35db2afc4203dc6431177ca7286a8a1
KhaoulaElkabtane <132408788+KhaoulaElkabtane@users.noreply.github.com>
2023-09-23 23:20:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -4,42 +4,42 @@ bus_t bus = {NULL, NULL, NULL, 0};
 * main - monty code interpreter
 * @argc: number of arguments
 * @argv: monty file location
-* Return: 0 on success
+* Return: True if 0 Faulse if 1 
 */
 int main(int argc, char *argv[])
 {
-	char *content;
-	FILE *file;
+	char *cont;
+	FILE *f;
 	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
+	ssize_t readLine = 1;
+	stack_t *stk = NULL;
+	unsigned int count = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
+	f = fopen(argv[1], "r");
+	bus.file = f;
+	if (!f)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-	while (read_line > 0)
+	while (readLine > 0)
 	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
+		cont = NULL;
+		readLine = getline(&content, &size, file);
+		bus.content = cont;
+		count++;
+		if (readLine > 0)
 		{
-			execute(content, &stack, counter, file);
+			execute(cont, &stk, count, f);
 		}
-		free(content);
+		free(cont);
 	}
-	free_stack(stack);
-	fclose(file);
+	frStack(stk);
+	fclose(f);
 return (0);
 }
