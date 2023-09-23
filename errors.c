1404caa98547a722240c19643ac32fc74edd8220
KhaoulaElkabtane <132408788+KhaoulaElkabtane@users.noreply.github.com>
2023-09-23 20:41:11 +0100

    Update errors.c

--- errors.c
+++ errors.c
@@ -2,7 +2,7 @@
 
 /**
  * err - Prints appropiate error messages determined by their error code.
- * @error_code: The error codes are the following:
+ * @errCode: The error codes are the following:
  * (1) => The user does not give any file or more than one file to the program.
  * (2) => The file provided is not a file that can be opened or read.
  * (3) => The file provided contains an invalid instruction.
@@ -12,32 +12,32 @@
  * (7) => When the stack it empty for pop.
  * (8) => When stack is too short for operation.
  */
-void err(int error_code, ...)
+void err(int errCode, ...)
 {
-	va_list ag;
-	char *op;
-	int l_num;
+	va_list lst;
+	char *str;
+	int num;
 
-	va_start(ag, error_code);
-	switch (error_code)
+	va_start(lst, errCode);
+	switch (errCode)
 	{
 		case 1:
 			fprintf(stderr, "USAGE: monty file\n");
 			break;
 		case 2:
 			fprintf(stderr, "Error: Can't open file %s\n",
-				va_arg(ag, char *));
+				va_arg(lst, char *));
 			break;
 		case 3:
-			l_num = va_arg(ag, int);
-			op = va_arg(ag, char *);
-			fprintf(stderr, "L%d: unknown instruction %s\n", l_num, op);
+			l_num = va_arg(lst, int);
+			str = va_arg(lst, char *);
+			fprintf(stderr, "L%d: unknown instruction %s\n", num, str);
 			break;
 		case 4:
 			fprintf(stderr, "Error: malloc failed\n");
 			break;
 		case 5:
-			fprintf(stderr, "L%d: usage: push integer\n", va_arg(ag, int));
+			fprintf(stderr, "L%d: usage: push integer\n", va_arg(lst, int));
 			break;
 		default:
 			break;
