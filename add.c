106026885f0b11dcf394af26c3fa106c497b9d04
KhaoulaElkabtane <132408788+KhaoulaElkabtane@users.noreply.github.com>
2023-09-23 23:45:32 +0100

    Update add.c

--- add.c
+++ add.c
@@ -21,7 +21,7 @@ void add(stack_t **hd, unsigned int count)
 		fprintf(stderr, "L%d: can't add, stack too short\n", count);
 		fclose(bus.file);
 		free(bus.content);
-		free_stack(*hd);
+		frStack(*hd);
 		exit(EXIT_FAILURE);
 	}
 	stc = *hd;
